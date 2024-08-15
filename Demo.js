f4c2065bab18a9dd5a3c79a31e7c0d9a181173a3
lehungthienan <anlehungthien@gmail.com>
2024-08-15 22:26:50 +0700

    BaiThucHanh
    baiThucHanh

---
+++ Demo.js
@@ -0,0 +1,7 @@
+function clickMe() {
+    //alert('Click me!');
+    document.getElementById("like").style.backgroundColor = "blue"
+    document.getElementById("like").style.color = "white"
+    document.getElementById("like").innerText = "Dislike"
+//     
+}
